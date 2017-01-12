a0194baec89b91e8a18132c51613cee6a0fb7027
david <david@kobal.ferelden>
2017-01-12 12:03:24 +0100

    Evolution du site web vitrine

--- modules/reperto/reperto.js
+++ modules/reperto/reperto.js
@@ -23,7 +23,7 @@
 			/** INITIALISATION */
 
 			this.display = {};
-			this.navigationmode = "tree";  // map | tree | print
+			this.navigationmode = "map";  // map | tree | print
 			
 			var guideid = "9";
 			
